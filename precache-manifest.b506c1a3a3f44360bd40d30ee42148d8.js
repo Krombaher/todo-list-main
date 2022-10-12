b0c6734c47c4c2c48836612e497a6c43f857d4e8
Vitalij Kazuro <vitalijkazuro939@gmail.com>
2022-10-12 16:31:10 +0300

    Updates

---
+++ precache-manifest.b506c1a3a3f44360bd40d30ee42148d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08109b362adeb1f91473451a112cc6f9",
+    "url": "/todo-list-main/index.html"
+  },
+  {
+    "revision": "7660d1f9509321f25145",
+    "url": "/todo-list-main/static/css/main.b92de430.chunk.css"
+  },
+  {
+    "revision": "ebd70be8ad4f05b60659",
+    "url": "/todo-list-main/static/js/2.8d17ff05.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todo-list-main/static/js/2.8d17ff05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7660d1f9509321f25145",
+    "url": "/todo-list-main/static/js/main.6f5f7d83.chunk.js"
+  },
+  {
+    "revision": "747d94dca6da4b143c33",
+    "url": "/todo-list-main/static/js/runtime-main.4668dbb0.js"
+  }
+]);
